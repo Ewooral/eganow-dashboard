89fe1fa26fd22d225ff9299a9a3929d3f0c98d2b
Maclean Ayarik <156457399+maclean-eganow@users.noreply.github.com>
2024-02-29 10:21:05 +0000

    fix submodule issues

--- protoc-generate.sh
+++ protoc-generate.sh
@@ -2,7 +2,8 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 # Path to Protoc Plugin
-#PROTOC_GEN_TS_PATH="${ROOT_DIR}/node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_TS_PATH="${ROOT_DIR}/node_modules/.bin/protoc-gen-ts"
+#PROTOC_GEN_TS_PATH="./node_modules/ts-protoc-gen/bin/protoc-gen-ts"
 
 # Directory holding all .proto files
 SRC_DIR="${ROOT_DIR}/src/protos/raw"
@@ -33,9 +34,19 @@ mkdir "${OUT_DIR}"
 
 # Generate all messages
 protoc \
-    --proto_path="${SRC_DIR}" $(find "${SRC_DIR}" -name "*.proto") \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
-    --grpc-web_out=import_style=typescript,mode=grpcweb:${OUT_DIR}
+    -I=$SRC_DIR $(find $SRC_DIR -iname "*.proto") \
+   --plugin=protoc-gen-ts="${PROTOC_GEN_TS_PATH}" \
+   --js_out=import_style=commonjs,binary:${OUT_DIR} \
+   --ts_out=service=grpc-web:${OUT_DIR}
+
+   # --grpc-web_out=import_style=typescript,mode=grpcweb:${OUT_DIR}
 
 
-#import_style=typescript
+
+
+# generate new files
+#protoc \
+ # -I=$PROTO_PATH $(find $PROTO_PATH -iname "*.proto") \
+ # --plugin=protoc-gen-ts="${PROTOC_GEN_TS_PATH}" \
+ # --js_out=import_style=commonjs,binary:${OUT_DIR} \
+ # --ts_out=service=grpc-web:${OUT_DIR}
