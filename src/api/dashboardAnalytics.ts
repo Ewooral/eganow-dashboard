48e8d212c1b053a1e0c60e2edc3c00d0f0aded1f
FaisalEganow <faisal@eganow.com>
2025-01-23 13:53:51 +0000

    -making typo corrections, - adding company ypes enum to constants

--- src/api/dashboardAnalytics.ts
+++ src/api/dashboardAnalytics.ts
@@ -1,8 +1,8 @@
-import BizCOllectInstance from '@/apiInstances/biz-collect-instance'
+import BizCollectInstance from '@/apiInstances/biz-collect-instance'
 import { CredentialsType } from '@/types/CredentialsType'
 
 export default function dashboardAnalytics (){
-    const axios = BizCOllectInstance()
+    const axios = BizCollectInstance()
     return {
         getDashboard : ()=>axios.get('/api/v1/merchant-transactions/dashboard')
     }
