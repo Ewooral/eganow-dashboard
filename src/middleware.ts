30c3c46fa1bd509c0d5baefa6d54350b08e8cb3e
ewooral <boahen.cos@gmail.com>
2025-03-11 17:10:14 +0000

    comment middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,43 @@
-import { NextResponse, NextRequest } from 'next/server'
-/* CONSTANTS */
-import { EGANOW_AUTH_COOKIE } from '@/constants'
-import { RequestCookie } from 'next/dist/server/web/spec-extension/cookies'
-
-/*
-*
-*
-  MIDDLEWARE
-*
-*
-*/
-
-export function middleware(request: NextRequest) {
-  try {
-    const auth_cookie: RequestCookie | undefined = request.cookies.get(EGANOW_AUTH_COOKIE)
-    const auth = typeof auth_cookie === 'object' ? JSON.parse(auth_cookie?.value) : undefined
-    //Checking if the user is logged in.
-    if (auth.accessToken) {
-      return NextResponse.next()
-    }
-    //If the user is not logged in then redirect to the login page.
-    return NextResponse.redirect(new URL('/login', request.url))
-  } catch (error) {
-    //Logging error to console.
-    console.error(error)
-    //If the user is not logged in then redirect to the login page.
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-}
-
-/* export const config = {
-   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/'],
-}
- */
-
-export const config = {
-  matcher: [
-    '/',
-    '/biz-collect/:path*',
-    '/business-details/:path*',
-  ],
-}
+// import { NextResponse, NextRequest } from 'next/server'
+// /* CONSTANTS */
+// import { EGANOW_AUTH_COOKIE } from '@/constants'
+// import { RequestCookie } from 'next/dist/server/web/spec-extension/cookies'
+//
+// /*
+// *
+// *
+//   MIDDLEWARE
+// *
+// *
+// */
+//
+// export function middleware(request: NextRequest) {
+//   try {
+//     const auth_cookie: RequestCookie | undefined = request.cookies.get(EGANOW_AUTH_COOKIE)
+//     const auth = typeof auth_cookie === 'object' ? JSON.parse(auth_cookie?.value) : undefined
+//     //Checking if the user is logged in.
+//     if (auth.accessToken) {
+//       return NextResponse.next()
+//     }
+//     //If the user is not logged in then redirect to the login page.
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   } catch (error) {
+//     //Logging error to console.
+//     console.error(error)
+//     //If the user is not logged in then redirect to the login page.
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   }
+// }
+//
+// /* export const config = {
+//    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/'],
+// }
+//  */
+//
+// export const config = {
+//   matcher: [
+//     '/',
+//     '/biz-collect/:path*',
+//     '/business-details/:path*',
+//   ],
+// }
